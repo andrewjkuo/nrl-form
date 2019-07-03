3a3d2c9f2e3ba8e2f931c5b5696d27de87a6256f
Andrew Kuo <andrewkuo@Andrews-iMac.local>
2019-07-03 23:52:54 +1000

    Luke fucks with your shit

--- static/nrl.js
+++ static/nrl.js
@@ -36,6 +36,12 @@ var team_img = [
     ['Wests Tigers','Tigers.gif', false]
 ]
 
+$(function() {
+    $('#date-select').change(function() {
+        updateGraph($(this).val());
+    });
+});
+
 var margin = {top: 30, right: 20, bottom: 40, left: 50},
 width = 900 - margin.left - margin.right,
 height = 600 - margin.top - margin.bottom;
@@ -48,149 +54,67 @@ var xAxis = d3.axisBottom(x);
 var yAxis = d3.axisLeft(y).tickValues([]);
 
 var valueline = d3.line()
-    .x(function(d) { return x(d.round); })
-    .y(function(d) { return y(d.form); })
-    .curve(d3.curveCardinal);
+.x(function(d) { return x(d.round); })
+.y(function(d) { return y(d.form); })
+.curve(d3.curveCardinal);
 
 var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+.attr("class", "tooltip")
+.style("opacity", 0);
 
-    var svg = d3.select("#chartArea")
-        .attr(
-          "style",
-          "padding-bottom: " + Math.ceil(height * 100 / width) + "%"
-        )
-        .append("svg")
-        .attr("viewBox", "0 0 " + (width+margin.left+margin.right) + " " + (height+margin.top+margin.bottom))
-            .style("background-color", "#C5C5C5")
-            .style("border-radius", "12px")
-        .append("g")
-            .attr("transform",
-                  "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#chartArea")
+.append("svg")
+.attr("viewBox", "0 0 " + (width+margin.left+margin.right) + " " + (height+margin.top+margin.bottom))
+.style("border-radius", "12px")
+.append("g")
+.attr("transform",
+"translate(" + margin.left + "," + margin.top + ")");
 
-    d3.select("#chartArea").selectAll('rect')
-        .data(team_img)
-        .enter()
-        .append("img")
-            .attr('class', 'nrl_logo')
-            .attr('id', function(d){ return 'logo'+d[0].replace(/\s+/g, '');})
-              .attr('src', function (d) {
-                  return 'static/img/team_logos/'+d[1];
-              })
-              .attr('width', '70px')
-              .style('border', '1px solid #999999')
-              .style('border-radius', '8px')
-              .style('padding', '2px')
-              .on("click", function(d) {
-                  if (d[2]) {
-                      for (var i = 0; i < team_img.length; i++) {
-                          team_img[i][2] = false;
-                      }
-                      d3.selectAll(".line")
-                          .style("opacity", 1)
-                          .style("stroke-width", 3);
-                      d3.selectAll(".nrl_logo")
-                          .style('border', '1px solid #999999');
-                  } else {
-                      for (var i = 0; i < team_img.length; i++) {
-                          team_img[i][2] = false;
-                      }
-                      d[2] = true;
-                  d3.selectAll(".line")
-                      .style("opacity", 0.3)
-                      .style("stroke-width", 2);
-                  d3.select("#tag"+d[0].replace(/\s+/g, ''))
-                      .style("stroke-width", 5)
-                      .style("opacity", 1);
-                    d3.selectAll(".nrl_logo")
-                        .style('border', '1px solid #999999');
-                    d3.select("#logo"+d[0].replace(/\s+/g, ''))
-                        .style("border", "5px solid rgba(0, 140, 186, 0.5)");
-                    }
-              });
+d3.select("#nrl-buttons").selectAll('rect')
+.data(team_img)
+.enter()
+.append("img")
+.attr('class', 'nrl_logo')
+.attr('id', function(d){ return 'logo'+d[0].replace(/\s+/g, '');})
+.attr('src', function (d) {
+    return 'static/img/team_logos/'+d[1];
+})
+.attr('width', '70px')
+.style('border', '1px solid #999999')
+.style('border-radius', '8px')
+.style('padding', '2px')
+.on("click", function(d) {
+    if (d[2]) {
+        for (var i = 0; i < team_img.length; i++) {
+            team_img[i][2] = false;
+        }
+        d3.selectAll(".line")
+        .style("opacity", 1)
+        .style("stroke-width", 3);
+        d3.selectAll(".nrl_logo")
+        .style('border', '1px solid #999999');
+    } else {
+        for (var i = 0; i < team_img.length; i++) {
+            team_img[i][2] = false;
+        }
+        d[2] = true;
+        d3.selectAll(".line")
+        .style("opacity", 0.3)
+        .style("stroke-width", 2);
+        d3.select("#tag"+d[0].replace(/\s+/g, ''))
+        .style("stroke-width", 5)
+        .style("opacity", 1);
+        d3.selectAll(".nrl_logo")
+        .style('border', '1px solid #999999');
+        d3.select("#logo"+d[0].replace(/\s+/g, ''))
+        .style("border", "5px solid rgba(0, 140, 186, 0.5)");
+    }
+});
 
-
-    d3.json("https://nrlform.appspot.com/v1/nrlform").then(function(data) {
-        data = data.filter(function(d) {
-            return d.year == 2019;
-        });
-        data.forEach(function(d) {
-            d.round = +d.round;
-            d.form = +d.form;
-        });
-        x.domain(d3.extent(data, function(d) { return d.round}))
-        y.domain([d3.min(data, function(d) {return d.form}), d3.max(data, function(d) {return d.form})])
-
-        var dataNest = d3.nest()
-            .key(function(d) { return d.team;})
-            .entries(data);
-
-        dataNest.forEach(function(d, i) {
-            svg.append("path")
-                .attr("class", "line")
-                .attr("id", 'tag'+d.key.replace(/\s+/g, ''))
-                .attr("d", valueline(d.values))
-                .style("stroke", team_col[d.values[0].team])
-                .on("mouseover", function() {
-                    d3.selectAll(".line")
-                        .style("opacity", 0.3)
-                        .style("stroke-width", 2);
-                    d3.select("#tag"+d.key.replace(/\s+/g, ''))
-                        .style("stroke-width", 5)
-                        .style("opacity", 1);
-                    div.style("opacity", .9);
-                    div.html(d.values[0].team)
-                        .style("left", (d3.event.pageX) + "px")
-                        .style("top", (d3.event.pageY - 28) + "px");
-                    for (var i = 0; i < team_img.length; i++) {
-                        team_img[i][2] = false;
-                    }
-                    d3.selectAll(".nrl_logo")
-                        .style('border', '1px solid #999999');
-                })
-                .on("mouseout", function(d) {
-                    div.transition()
-                        .duration(300)
-                        .style("opacity", 0);
-                    d3.selectAll(".line")
-                        .style("opacity", 1)
-                        .style("stroke-width", 3);
-                });
-        });
-
-		svg.append("g")
-            .attr("class", "xaxis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.append("text")
-            .attr("transform",
-                    "translate(" + (width/2) + " ," +
-                                   (height + margin.top) + ")")
-            .style("text-anchor", "middle")
-            .text("Round");
-
-        svg.append("g")
-            .attr("class", "yaxis")
-            .call(yAxis);
-
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - (margin.left / 2))
-            .attr("x",0 - (height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Form");
-    });
-
-function updateGraph(year) {
-    d3.select("h3")
-        .text(year);
 
 d3.json("https://nrlform.appspot.com/v1/nrlform").then(function(data) {
     data = data.filter(function(d) {
-        return d.year == year;
+        return d.year == 2019;
     });
     data.forEach(function(d) {
         d.round = +d.round;
@@ -200,25 +124,100 @@ d3.json("https://nrlform.appspot.com/v1/nrlform").then(function(data) {
     y.domain([d3.min(data, function(d) {return d.form}), d3.max(data, function(d) {return d.form})])
 
     var dataNest = d3.nest()
+    .key(function(d) { return d.team;})
+    .entries(data);
+
+    dataNest.forEach(function(d, i) {
+        svg.append("path")
+        .attr("class", "line")
+        .attr("id", 'tag'+d.key.replace(/\s+/g, ''))
+        .attr("d", valueline(d.values))
+        .style("stroke", team_col[d.values[0].team])
+        .on("mouseover", function() {
+            d3.selectAll(".line")
+            .style("opacity", 0.3)
+            .style("stroke-width", 2);
+            d3.select("#tag"+d.key.replace(/\s+/g, ''))
+            .style("stroke-width", 5)
+            .style("opacity", 1);
+            div.style("opacity", .9);
+            div.html(d.values[0].team)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+            for (var i = 0; i < team_img.length; i++) {
+                team_img[i][2] = false;
+            }
+            d3.selectAll(".nrl_logo")
+            .style('border', '1px solid #999999');
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+            .duration(300)
+            .style("opacity", 0);
+            d3.selectAll(".line")
+            .style("opacity", 1)
+            .style("stroke-width", 3);
+        });
+    });
+
+    svg.append("g")
+    .attr("class", "xaxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+    svg.append("text")
+    .attr("transform",
+    "translate(" + (width/2) + " ," +
+    (height + margin.top) + ")")
+    .style("text-anchor", "middle")
+    .text("Round");
+
+    svg.append("g")
+    .attr("class", "yaxis")
+    .call(yAxis);
+
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - (margin.left / 2))
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Form");
+});
+
+function updateGraph(year) {
+
+    d3.json("https://nrlform.appspot.com/v1/nrlform").then(function(data) {
+        data = data.filter(function(d) {
+            return d.year == year;
+        });
+        data.forEach(function(d) {
+            d.round = +d.round;
+            d.form = +d.form;
+        });
+        x.domain(d3.extent(data, function(d) { return d.round}))
+        y.domain([d3.min(data, function(d) {return d.form}), d3.max(data, function(d) {return d.form})])
+
+        var dataNest = d3.nest()
         .key(function(d) { return d.team;})
         .entries(data);
 
-    var svg = d3.select("#chartArea").transition();
+        var svg = d3.select("#chartArea").transition();
 
-    dataNest.forEach(function(d, i) {
-        svg.select("#"+'tag'+d.key.replace(/\s+/g, ''))
+        dataNest.forEach(function(d, i) {
+            svg.select("#"+'tag'+d.key.replace(/\s+/g, ''))
             .duration(750)
             .attr("d", valueline(d.values))
-            });
+        });
 
         svg.select(".xaxis")
-            .transition(500)
-            .call(xAxis);
+        .transition(500)
+        .call(xAxis);
 
         svg.select(".yaxis")
-            .transition(500)
-            .call(yAxis)
-            .selectAll("text")
-            .remove();
+        .transition(500)
+        .call(yAxis)
+        .selectAll("text")
+        .remove();
     });
 };
